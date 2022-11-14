726f2af6d3c6a4da495803160567dc18d4773d5c
Andre Rousselet <andre098@hotmail.com>
2022-11-14 13:39:13 -0300

    feat(chat room): add auto scroll down when message sent functionality

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,3 +1,4 @@
+import { useRef, useEffect } from 'react';
 import { firestore } from '../firebaseInit';
 import { collection, query, orderBy, limit } from "firebase/firestore";
 import { useCollectionData } from 'react-firebase-hooks/firestore';
@@ -5,6 +6,8 @@ import ChatMessage from './ChatMessage';
 import SendMessage from './SendMessage';
 
 export default function ChatRoom() {
+  const dummy = useRef();
+
   const messagesCollection = collection(firestore, 'messages');
   const messagesQuery = query(
     messagesCollection,
@@ -15,17 +18,23 @@ export default function ChatRoom() {
   const [messages, loading, error] = useCollectionData(
     messagesQuery,
     { idField: 'id' },
-    );
+  );
+  
+  useEffect(() => {
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }, [messages])
 
   return (
     <>
-      <main>
+      <main className='container mt-5'>
         {error && <strong>Error: {JSON.stringify(error)}</strong>}
         {loading && <span>Collection: Loading...</span>}
         <section>
           {messages?.map((msg) => (
             <ChatMessage key={ msg.createdAt } message={ msg }/>
           ))}
+
+          <span ref={ dummy }></span>
         </section>
       </main>
       <SendMessage messagesCollection={ messagesCollection }/>
