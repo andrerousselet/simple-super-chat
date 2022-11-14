50d622ec52baa62725f9c006e413a71e192f623e
Andre Rousselet <andre098@hotmail.com>
2022-11-14 15:09:33 -0300

    feat(chat message): add constant to display messages on screen on the roght or left
    depending on user

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,15 +1,20 @@
+import { auth } from '../firebaseInit';
+
 export default function ChatMessage({ message }) {
-  const { text, createdAt, photoURL } = message;
+  const { uid, text, createdAt, photoURL } = message;
   const date = createdAt.toDate().toLocaleString();
+
+  const messageClass = uid === auth.currentUser.uid ? 'flex-row-reverse' : '';
+
   return (
-    <div className='d-flex align-items-center gap-2'>
+    <div className={ `d-flex gap-2 my-2 ${messageClass} `}>
       <img
-        className='rounded-circle'
+        className='rounded-circle mt-1'
         style={ { width: '40px', height: '40px' } }
         src={ photoURL }
         alt="user"
       />
-      <div className='border rounded-4 p-1 my-2 border-opacity-10'>
+      <div className='border rounded-3 p-1 px-2 border-opacity-10'>
         <p
           className='card-subtitle text-muted'
           style={ { fontSize: '12px' } }
