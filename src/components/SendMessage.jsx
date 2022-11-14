145a40827369d8b4259d166da28a92bd98d7756f
Andre Rousselet <andre098@hotmail.com>
2022-11-14 14:45:00 -0300

    feat(photourl): include photoURL from user to SendMessage component

--- src/components/SendMessage.jsx
+++ src/components/SendMessage.jsx
@@ -7,11 +7,12 @@ export default function Footer({ messagesCollection }) {
 
   const sendMessage = async (e) => {
     e.preventDefault();
-    const { uid } = auth.currentUser;
+    const { uid, photoURL } = auth.currentUser;
     const newMessage = {
       uid,
       text: message,
       createdAt: Timestamp.now(),
+      photoURL,
     }
     await addDoc(messagesCollection, newMessage);
     setMessage('');
@@ -19,11 +20,12 @@ export default function Footer({ messagesCollection }) {
 
   return (
     <form
-      className='d-flex justify-content-between p-2 fixed-bottom'
+      className='d-flex bg-secondary justify-content-between p-2 mt-5 fixed-bottom'
       onSubmit={ sendMessage }
     >
       <input
         className='w-100 form-control me-2'
+        style={ { backgroundColor: '#adb5bd' } }
         type='text'
         placeholder='Write your message here...'
         value={ message }
